7ca7ff0dabe6849f2c13b0e08baaa2dfa3abf1a1
Shashank Singh <82604413+shashanksingh2002@users.noreply.github.com>
2023-04-24 02:47:55 +0530

    Add files via upload

---
+++ backroad_app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {Navigation} from './components/Header';
+import {Home} from './components/Home'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Navigation/>
+    <Home/>
+  </React.StrictMode>
+);
